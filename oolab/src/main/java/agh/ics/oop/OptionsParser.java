e20ec7b5a119ffd64ae432f4f1a46991b14c063d
Michał Plaza <michalplaza9@gmail.com>
2023-10-16 13:46:19 +0200

    dodano oolab, instrukcje sterujące zwierzaka

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,32 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+
+public class OptionsParser {
+    public static MoveDirection[] parse(String[] args) {
+        int length = args.length;
+        MoveDirection[] directions = new MoveDirection[length];
+
+        for (int i = 0; i < length; i++) {
+            switch (args[i]) {
+                case "f":
+                    directions[i] = MoveDirection.FORWARD;
+                    break;
+                case "b":
+                    directions[i] = MoveDirection.BACKWARD;
+                    break;
+                case "r":
+                    directions[i] = MoveDirection.RIGHT;
+                    break;
+                case "l":
+                    directions[i] = MoveDirection.LEFT;
+                    break;
+                default:
+                    // Ignore invalid arguments
+                    break;
+            }
+        }
+
+        return directions;
+    }
+}
